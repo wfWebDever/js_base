9b284971d3fc46283fb08c6a2875b73a986da2be
weifeng <xinyilan2005@gmail.com>
2019-05-19 19:55:47 +0800

    #通过proxy改造

--- deepclone.js
+++ deepclone.js
@@ -1,4 +1,4 @@
-const obj = { a: [1, 2], b: { c: { d: 1 }, e: 2 } };
+const obj = {a: [1, 2], b: {c: {d: 1}, e: 2}};
 const _toString = Object.prototype.toString;
 const isFunction = function (obj) {
   return _toString.call(obj) === "[object Function]";
