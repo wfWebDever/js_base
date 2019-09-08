6cba10dbb8ec288536b8fd92450295127d1a50fb
weifeng <xinyilan2005@gmail.com>
2019-09-08 21:55:53 +0800

    save

---
+++ Queue.js
@@ -0,0 +1,19 @@
+//  构造队列 先进先出
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+  enQueue(element) {
+    this.items.push(element);
+  }
+  deQueue() {
+    return this.items.shift();
+  }
+  isEmpty() {
+    return this.items;
+  }
+  front() {
+    return this.items[0];
+  }
+}
+export default Queue;
