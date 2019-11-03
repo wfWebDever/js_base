ea71ee30a87de2e71eca915c3880dd7b7a51172b
weifeng <xinyilan2005@gmail.com>
2019-11-03 22:01:55 +0800

    save

--- string.js
+++ string.js
@@ -1,25 +1,15 @@
-﻿
+﻿//let str="  sds ";
+//console.info("--"+str.trim()+"--");
 
 
-var str="  sds "
-console.info("--"+str.trim()+"--");
+String.prototype.trim = function () {
+  const reg = new RegExp('(^\\s*)|(\\s*$)', 'g');
+  return this.replace(reg, "");
+};
 
-console.info(str.charAt(2));
+let str2 = "Hello ";
+let str3 = "Word ";
+let str4 = " job! ";
 
-var str1="  aaa ff ";
-console.info("|"+str1.trim()+"|");
-	
-
-
-String.prototype.trim=function() {
-	var regular=/(^\s*)|(\s*$)/gi;
-	return(this.replace(regular,""));
-}
-
-var str2="Hello ";
-var str3="Word ";
-var str4=" job!";
-console.info(str2.concat(str3,str4));
-console.info(str2.substring(0,2));
-//ddd
+console.log('--' + str4.trim() + '--');
 
