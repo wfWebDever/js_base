c83c939f594725cc848725cbba4aa18e4ea99146
weifeng <xinyilan2005@gmail.com>
2020-02-25 22:18:51 +0800

    #0225 promise

--- ES6/promise.js
+++ ES6/promise.js
@@ -1,25 +1,38 @@
 // 手写实现一个promise
+const PENDING = 'pending'
+const FULLFILEd = 'fullfiled'
+const REJECTEd = 'rejected'
 class MyPromise {
-  value = null
-  status = null
-  pedding = 'pedding'
-  fullfiled = 'fullfiled'
-  rejected = 'rejected'
-  
+  // status = PENDING; // ES7提案，需要babel
+  // value = undefined;
   constructor (executor) {
-    executor(this.resolve.bind(this), this.reject);
+    this.init()
+    executor(this.resolve.bind(this), this.reject.bind(this))
   }
-  
+
+  init () {
+    this.status = PENDING
+    this.value = undefined
+  }
+
   resolve (value) {
+    this.status = FULLFILEd
+    this.value = value
+  }
+
+  reject (error) {
+    this.status = REJECTEd
+    this.value = error
+  }
+
+  then () { // then需要透传数据
   
   }
 }
 
 const promise = new MyPromise(function (resolve, reject) {
-  let a = 1;
-  resolve(1);
+  resolve(1)
 })
-
-const promise2 = new Promise((resolve, reject) => {
-  resolve(1);
+promise.then((data) => {
+  console.log(data)
 })
