3e2bf757337227535b15536a7e2dae1bcb64ec1c
weifeng <xinyilan2005@gmail.com>
2020-07-05 22:16:26 +0800

    #0705

--- 算法/test.js
+++ 算法/test.js
@@ -1,19 +1,34 @@
-const test = -121
-
-const isPalindrome = function (x) {
-  x = x.toString()
-  const xArr = x.split('')
-  if (xArr.length === 0) {
-    return false
+var isValid = function(s) {
+  if (typeof s !== 'string') {
+    return  false
   }
-  if (xArr.length === 1) {
+  if (s === '') {
     return true
   }
-  if (xArr.length === 2) {
-    return xArr[0] === xArr[1]
+  if (s.length % 2 !== 0) {
+    return  false
   }
-  const leftStr = xArr.slice(0, Math.floor(xArr.length / 2)).join('')
-  const rightStr = xArr.slice(Math.ceil(xArr.length / 2)).reverse().join('')
-  return leftStr === rightStr
-}
-console.log(isPalindrome(test))
+  const arr = s.split('')
+  const stack = [arr[0]]
+  const match = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === match[stack[stack.length - 1]]) {
+      stack.pop()
+    } else {
+      stack.push(arr[i])
+    }
+  }
+  return stack.length === 0
+  
+};
+const data1 = "()"
+const data2 = "()[]{}"
+const data3 = "(]"
+const data4 = "([)]"
+const data5 = "{[]}"
+
+console.log(isValid(data1), isValid(data2), isValid(data3), isValid(data4), isValid(data5), isValid('(){'))
