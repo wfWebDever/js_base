30afd88443467c98971d9d5d43da16dd22108a05
weifeng <xinyilan2005@gmail.com>
2020-09-06 21:43:41 +0800

    #0906 整理

---
+++ config/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/main.js'),
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
