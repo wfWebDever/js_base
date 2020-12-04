ce19a43ba2e4b545cada0bbc46c1db9757a5d3d0
weifeng <xinyilan2005@gmail.com>
2020-12-04 23:06:55 +0800

    #1204

--- ES6/promise-all.js
+++ ES6/promise-all.js
@@ -2,31 +2,28 @@
 * promise all
 * */
 const promiseAll = function (arr) {
-  // return [data1, data2, data3]
-  if (typeof arr !== 'array') {
+  if (!Array.isArray(arr)) {
     throw new ReferenceError(`${arr} must is a array`)
   }
-  // 为空时立即返回promise
   if (!arr.length) return Promise.resolve([])
   const promiseArr = arr.map(item => {
-    if (item instanceof Promise) {
-      return item
-    } else {
-      return Promise.resolve(item)
-    }
+    return item instanceof Promise ? item : Promise.resolve(item)
   })
   
-  const arrData = new Array(promiseArr.length)
-  const len = arrData.length
+  const result = new Array(promiseArr.length)
+  let len = 0
+  let hasCatch = false
   return new Promise((resolve, reject) => {
     promiseArr.forEach( (item, index) => {
       item.then(data => {
-        arrData[index] = data
-        if (index === len - 1) {
-          return resolve(arrData)
+        result[index] = data
+        len++
+        if (len === promiseArr.length) {
+          resolve(result)
         }
       }).catch(error => {
-        return reject(error)
+        !hasCatch && reject(error)
+        hasCatch = true
       })
     })
   })
