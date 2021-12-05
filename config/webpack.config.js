d45c294811abea52ddcb749e7c97b5148742b66a
wfwebdev <xinyilan2005@gmail.com>
2021-12-05 21:23:25 +0800

    'webpack'

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const rootPath = '../'
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, './src/main.js'),
+  entry: {
+    index: path.resolve(__dirname, rootPath, 'src/main.js'),
+  },
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: '[name].[hash:8]js',
+    filename: '[name].[hash:8].js',
     // 输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, rootPath, 'dist'),
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,1 @@
-// 通过 CommonJS 规范导入 show 函数
-const show = require('./show.js');
-// 执行 show 函数
-show('Webpack');
+console.log('test ', 'webpack ')
