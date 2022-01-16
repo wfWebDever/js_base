cb32334223858ca98190f6733d2413aa6259999a
wfwebdev <xinyilan2005@gmail.com>
2022-01-16 15:39:42 +0800

    save

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
 // import Libss from './libsss'
+import('./libsss').then(() => {
+    console.log('libss', 'done')
+})
 console.log('test ', 'webpack ')
 console.log(React, useState)
