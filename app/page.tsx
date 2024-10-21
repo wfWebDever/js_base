de616b72aa957cae04c9634fb368ea81e2f1a47f
Feng Wei <xinyilan2005@gmail.com>
2024-10-21 10:21:50 +0800

    docs: update reflection

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,35 @@ const data = [
 
 export default function Page() {
   return (
-    <section>
-      <h2 className="font-semibold">Latest Posts</h2>
-      <Catalog data={data} />
-    </section>
+    <>
+      <h1 className="font-semibold">Reflections</h1>
+      <p className="my-4">
+        Many frontend features that were previously built using complex tricks can now be
+        implemented with basic principles due to the development of browsers and frontend
+        technologies.
+      </p>
+      <p className="my-4">
+        For example, layouts that used to require many hacks can now be achieved simply by using
+        `flex` and `grid`.
+      </p>
+      <p className="my-4">
+        Another example is the floating header at the top, which used to require various positioning
+        techniques but can now be done with `sticky`.
+      </p>
+      <p className="my-4">
+        Sometimes I wonder if we&apos;d be better off just waiting for technological advancements
+        rather than racking our brains to come up with laborious methods to create effects that look
+        cool but aren&apos;t really worth the effort.{' '}
+      </p>
+      <p className="my-4">
+        Another example is the interview questions and algorithm problems we used to compile. Now
+        with the support of AI, we just need to input the questions, and it can generate answers,
+        and the quality of the answers is quite good.
+      </p>
+      <section>
+        <h2 className="font-semibold">Latest Posts</h2>
+        <Catalog data={data} />
+      </section>
+    </>
   )
 }
