1da7f5ea621faa68a44935232f5b91f9580a308c
Feng Wei <xinyilan2005@gmail.com>
2024-10-24 17:04:25 +0800

    refactor: update eslint, vscode, next.config.mjs, tsconfig.json, package.json

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@ import withMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const config = withMDX()({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx']
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true
+  }
 })
 
 export default config
