6343a6f531296bf8e0a800ea3b012a262c224597
Feng Wei <xinyilan2005@gmail.com>
2024-10-25 17:06:51 +0800

    docs: add setting up new computer for development

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,9 +4,9 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    h1: ({ children }) => <h1 className="font-semibold text-3xl text-black mt-1">{children}</h1>,
+    h1: ({ children }) => <h1 className="font-semibold text-3xl text-black">{children}</h1>,
     h2: ({ children }) => (
-      <h2 className="font-semibold text-2xl text-black mb-6 mt-12">{children}</h2>
+      <h2 className="font-semibold text-2xl text-black mb-6 mt-12 ">{children}</h2>
     ),
     h3: ({ children }) => (
       <h3 className="font-semibold text-base text-black mb-5 my-4">{children}</h3>
