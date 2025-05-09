ec25929ff78eea59f240943fb658b6ddbb0c55ef
Feng Wei <xinyilan2005@gmail.com>
2025-05-09 17:04:06 +0800

    feat: add reflections for 2024 and 40-year-old

---
+++ app/reflections/page.tsx
@@ -0,0 +1,23 @@
+import Catalog from '@/components/catalog'
+
+const data = [
+  {
+    title: '经验累积',
+    description: '2024年感想',
+    href: '/reflections/2024-thoughts'
+  },
+  {
+    title: '四十不惑?',
+    description: '',
+    href: '/reflections/40-year-old'
+  }
+]
+
+export default function BuildDevEnvironmentPage() {
+  return (
+    <section>
+      <h1 className="font-semibold">感悟&随想</h1>
+      <Catalog data={data} />
+    </section>
+  )
+}
